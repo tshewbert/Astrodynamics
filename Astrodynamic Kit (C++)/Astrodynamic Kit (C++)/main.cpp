94623504e9ba1b14ae7f58376b3cd01e9a28d493
tshewbert <tshewbert@gmail.com>
2015-03-03 10:22:37 -0800

    first creation

---
+++ Astrodynamic Kit (C++)/Astrodynamic Kit (C++)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Astrodynamic Kit (C++)
+//
+//  Created by Tyler Shewbert on 3/3/15.
+//  Copyright (c) 2015 Tyler Shewbert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Welcome to Tyler Shewbert's Astrodynamics Toolkit!\nThis is a work in progress to develop a set of tools to be used for astrodynamic calculations\nas a way to practice C++";
+    return 0;
+}
